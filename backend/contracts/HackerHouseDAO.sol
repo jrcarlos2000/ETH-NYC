380c35ab2a477c875796d71d33e2ee9ed95c9a54
Konrad Gnat <konradmgnat@gmail.com>
2022-06-25 18:15:23 -0400

    implement hhdao contract

--- backend/contracts/HackerHouseDAO.sol
+++ backend/contracts/HackerHouseDAO.sol
@@ -8,16 +8,21 @@ contract HackerHouseDAO {
 
     address public parentAddress;
     uint256 id;
+    Counters.Counter public userId;
     string public name;
     string public descriptionURI;
     address[] public coreTeam;
     uint256 public membershipPrice;
     bool public initialized = false;
+    mapping (address => User) public users;
+    address[] userAddresses;
 
-    struct member {
+    struct User {
         uint256 id;
-        address memberAddr;
+        bool exists;
+        address userAddr;
         bool isMember;
+        address[] approvers;
     }
 
     function initialize(
@@ -34,5 +39,40 @@ contract HackerHouseDAO {
         coreTeam = _coreTeam;
         name = _name;
         descriptionURI = _descriptionURI;
+    }
+
+    function applyForMembership() external {
+        require(!users[msg.sender].isMember, "already a member or applicants");
+        uint256 _memId = userId.current();
+        address[] memory _approvers;
+        User memory _user = User({
+            id: _memId,
+            exists: true,
+            userAddr: address(msg.sender),
+            isMember: false,
+            approvers: _approvers
+        });
+        users[msg.sender] = _user;
+        userAddresses.push(msg.sender);
+        userId.increment();
+    }
+
+    function fetchAllApplicants() public view returns (User[] memory applicantsArr) {
+        uint256 usersLen = userAddresses.length;
+        uint256 applicantsCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < usersLen; i++) {
+            if (!users[userAddresses[i]].isMember) {
+                applicantsCount++;
+            }
+        }
+        User[] memory applicantsArr = new User[](applicantsCount);
+        for (uint256 i = 0; i < usersLen; i++) {
+            if (!users[userAddresses[i]].isMember) {
+                applicantsArr[currentIndex] = (users[userAddresses[i]]);
+                currentIndex++;
+            }
+        }
     }
 }
